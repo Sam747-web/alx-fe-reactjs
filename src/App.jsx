32c9fad66f3ede6be9db7cc938435c0c56296da0
Samuel Amoah <samuelamoah075@gmail.com>
2025-02-27 00:08:40 -0800

    Removed the last committed file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from "./components/WelcomeMessage";
+
+function MyComponent() {  
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
+
