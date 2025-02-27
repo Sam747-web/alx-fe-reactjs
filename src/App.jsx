6fbeeec77e5d8498e4a90f3d632bd6b95139495f
Samuel Amoah <samuelamoah075@gmail.com>
2025-02-27 04:34:02 -0800

    Added UserProfile component using props

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,37 @@ function MyComponent() {
 }
 export default MyComponent;
 
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      {/* Existing component */}
+      <WelcomeMessage />
+
+      {/* New components */}
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+import React from "react";
+import UserProfile from "./components/UserProfile"; // Import the component
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+}
+
+export default App;
