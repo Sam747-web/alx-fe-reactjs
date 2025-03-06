06fde934613565d0c77c082ab3981615a4603866
Samuel Amoah <samuelamoah075@gmail.com>
2025-03-06 09:43:59 -0800

    Fixed errors in App.jsx and WelcomeMessage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import WelcomeMessage from "./components/WelcomeMessage";
-
-function MyComponent() {  
-  return (
-    <div>
-      <h1>Hello everyone, I am learning React at ALX!</h1>
-    </div>
-  );
-}
-export default MyComponent;
-
+import React from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  return (
-    <div>
-      {/* Existing component */}
-      <WelcomeMessage />
-
-      {/* New components */}
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
-
-import React from "react";
-import UserProfile from "./components/UserProfile"; // Import the component
-
-function App() {
-  return (
-    <div>
-      <h1>Welcome to My React App</h1>
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <WelcomeMessage />
+            <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
