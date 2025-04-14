dfb7c27606f0d68c1fc062c952cd13fb5ba99861
Samuel Amoah <samuelamoah075@gmail.com>
2025-04-14 19:58:38 +0000

    Set up React app inside alx-react-app directory

--- alx-react-app/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <footer><p>&copy; 2025 React App</p></footer>;
-};
-
-export default Footer;
--- alx-react-app/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <header><h1>React App Header</h1></header>;
-};
-
-export default Header;
